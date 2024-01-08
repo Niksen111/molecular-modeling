a6a78e1dea7b3c7b9b9836d960ce23c659154315
Niksen111 <nikiksen2003@gmail.com>
2024-01-08 23:35:12 +0300

    Interface updated

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import org.nksenchik.MolecularSystem
+import org.nksenchik.vo.History
 import java.lang.Exception
 import kotlin.math.PI
 import kotlin.math.sqrt
@@ -25,14 +26,11 @@ fun main(args: Array<String>) {
     var moleculesNumber = 3
     var stepsNumber = 1000
     var dt = 1e-10
-    var T: Double = 293.0
-    var sigma: Double = 3.54
-    var epsilon: Double = 0.00801
-    var m0: Double = 6.6335209e-26
+    var T = 293.0
+    var sigma = 3.54
+    var epsilon = 0.00801
+    var m0 = 6.6335209e-26
     var cubeSize: Double = 2 * sqrt(sigma / PI) * 50
-    var system = MolecularSystem(moleculesNumber)
-    var history = system.performSimulation(stepsNumber, dt)
-    var filtered = history.filter { histories -> histories.any { it.coordinates.x > -1 } }
     printInfo()
     printCommands()
     while (true) try {
@@ -49,23 +47,23 @@ fun main(args: Array<String>) {
                 printCommands()
             }
             "вводПарам" -> {
-                println("Нажмите Enter чтобы оставить значение по умолчанию")
-                println("Введите количество молекул в системе (по умолч. 3):")
-                moleculesNumber = readln().toIntOrNull() ?: 3
-                println("Введите количество шагов симуляции (по умолч. 1000):")
-                stepsNumber = readln().toIntOrNull() ?: 1000
-                println("Введите dt (временной шаг симуляции) (по умоч. 1e-10):")
-                dt = readln().toDoubleOrNull() ?: 1e-10
-                println("Введите температру системы (по умолч. 293.0):")
-                T = readln().toDoubleOrNull() ?: 293.0
-                println("Введите значение сигма (по умолч. 3.54):")
-                sigma = readln().toDoubleOrNull() ?: 3.54
-                println("Введите значение эпсилон (по умолч. 0.00801):")
-                epsilon = readln().toDoubleOrNull() ?: 0.00801
-                println("Введите массу молекулы (по умолч. 6.6335209e-26):")
-                m0 = readln().toDoubleOrNull() ?: 6.6335209e-26
-                println("Введите размер куба (по умолч. ${2 * sqrt(sigma / PI) * 50}):")
-                cubeSize = readln().toDoubleOrNull() ?: (2 * sqrt(sigma / PI) * 50)
+                println("Нажмите Enter чтобы оставить текущее значение")
+                println("Введите количество молекул в системе (текущее $moleculesNumber):")
+                moleculesNumber = readln().toIntOrNull() ?: moleculesNumber
+                println("Введите количество шагов симуляции (текущее $stepsNumber):")
+                stepsNumber = readln().toIntOrNull() ?: stepsNumber
+                println("Введите dt (временной шаг симуляции) (текущее $dt):")
+                dt = readln().toDoubleOrNull() ?: dt
+                println("Введите температру системы (текущее $T):")
+                T = readln().toDoubleOrNull() ?: T
+                println("Введите значение сигма (текущее $sigma):")
+                sigma = readln().toDoubleOrNull() ?: sigma
+                println("Введите значение эпсилон (текущее $epsilon):")
+                epsilon = readln().toDoubleOrNull() ?: epsilon
+                println("Введите массу молекулы (текущее $m0):")
+                m0 = readln().toDoubleOrNull() ?: m0
+                println("Введите размер куба (текущее $cubeSize):")
+                cubeSize = readln().toDoubleOrNull() ?: cubeSize
             }
             "печатьПарам" -> {
                 println("Количество молекул в системе: $moleculesNumber")
@@ -78,9 +76,9 @@ fun main(args: Array<String>) {
                 println("Размер куба: $cubeSize")
             }
             "старт" -> {
-                system = MolecularSystem(moleculesNumber, T, sigma, epsilon, m0, cubeSize)
-                history = system.performSimulation(stepsNumber, dt)
-                filtered = history.filter { histories -> histories.any { it.coordinates.x > -1 } }
+                val system = MolecularSystem(moleculesNumber, T, sigma, epsilon, m0, cubeSize)
+                val history = system.performSimulation(stepsNumber, dt)
+                val filtered = history.filter { histories -> histories.any { it.coordinates.x > -1 } }
                 system.drawTrajectories(filtered)
             }
             "выход" -> {
